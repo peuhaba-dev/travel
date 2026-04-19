7e01421d3435faac94f52dcf92d0b2ff61f24f5b
peuhaba-dev <peuhabashop@gmail.com>
2026-04-19 19:36:42 +0700

    Perubahan iklan adsense

--- app/city/[cityName]/page.tsx
+++ app/city/[cityName]/page.tsx
@@ -228,9 +228,9 @@ export default function CityPage({ params }: Props) {
       </section>
 
       {/* AdSense sticky mobile */}
-      <div className="sticky bottom-0 z-40 md:hidden bg-white border-t border-gray-200 shadow-lg">
-        <AdBanner slot="9988776655" format="auto" />
-      </div>
+      <div className="sticky bottom-0 z-40 md:hidden bg-white border-t border-gray-200 shadow-lg max-h-[90px] overflow-hidden">
+  <AdBanner slot="9988776655" format="horizontal" />
+</div>
     </div>
   )
 }
